9c3699fb89f63f67ae9ee2a9d9cf4b4cba068e67
adrianpaliz <adrianpaliz@gmail.com>
2022-02-27 20:00:01 -0500

    Change variables names and add more comments

--- main_loop.py
+++ main_loop.py
@@ -1,49 +1,52 @@
+#  Import libraries that we are going to use
 import pygame
 
-# Initialize
+# Initialize pygame
 pygame.init()
-# Set up screen
+
+# Set up screen and define it as a variable
 screen = pygame.display.set_mode((600, 800))
 
 game_over = False
 
-# Initial ball position
-position_on_x_axis = 300
-position_on_y_axis = 400
+# Initial ball position variables
+x_position = 300
+y_position = 400
 
-# Ball speed 
-x_axis_speed = 1
-y_axis_speed = 1
-
+# Ball speed variables
+x_speed = 1
+y_speed = 1
 
 # Main while loop
 while not game_over:
-    # Process events
-    events = pygame.event.get()    
+    # Process events and define it as a variable
+    events = pygame.event.get()   
+
     for event in events:
         if event.type == pygame.QUIT:
             game_over = True    
     
-    # Increased ball movement (Cartesian plane)
-    position_on_x_axis += x_axis_speed 
-    position_on_y_axis += y_axis_speed 
+    # Increased movement of the ball, inside the while loop
+    x_position += x_speed 
+    y_position += y_speed
 
-    #rebound
-    if position_on_x_axis >= 600 -10 or position_on_x_axis <= 0 + 10:
-        x_axis_speed *= -1
+    # If for rebound
+    # Subtract or add ten from the radius
+    if x_position >= 600 -10 or x_position <= 0 + 10:
+        # Multiply by minus one to change direction
+        x_speed *= -1
       
-    if position_on_y_axis >= 800 - 10 or position_on_y_axis <= 0 + 10:
-        y_axis_speed *= -1
-    
+    if y_position >= 800 - 10 or y_position <= 0 + 10:
+        y_speed *= -1    
 
-    # Refresh screen
+    # Print the screen
     screen.fill((255, 0, 0))
 
-    #Game objects modification
-    ball = pygame.draw.circle(screen,(255, 255, 0), (position_on_x_axis, position_on_y_axis), 10)
-    #Commented line for basic debuging the ball position
-    #print(x, y)
+    # Game objects modification
+    ball = pygame.draw.circle(screen,(255, 255, 0), (x_position, y_position), 10)    
     
+    # Refresh the screen
     pygame.display.flip()
 
+# Desactives the pygame library
 pygame.quit()
